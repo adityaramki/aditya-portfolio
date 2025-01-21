c647450c3f18200f5e34af39ec17e71f7dfb9ccc
adityaramki <aditya.ramki1@gmail.com>
2025-01-21 12:03:38 -0500

    added projects page

---
+++ src/components/Projects/index.js
@@ -0,0 +1,56 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+
+
+
+const Projects = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+    
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+    return (
+        <>
+            <div className='container projects-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                        letterClass={letterClass} 
+                            strArray = {['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+                            idx = {10}
+                        />
+                    </h1>
+                    <h2>Coming Soon...</h2>
+                    <div className='github'>
+                        <ul>
+                            <li>
+                                <a target="_blank" rel='noreferrer' href='https://github.com/adityaramki'>
+                                    <FontAwesomeIcon icon={faGithub} color="#000000" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                        
+                </div>
+            </div>
+            <Loader type='line-scale-pulse-out' color="#16eec0"/>
+        </>
+        
+        
+    )
+
+
+}
+    
+export default Projects
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
@@ -15,6 +15,9 @@ const Sidebar = () => (
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+                <FontAwesomeIcon icon={faCodeBranch} color="#4d4d4e" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
