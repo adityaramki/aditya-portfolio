c28e9fbdeb732561db4fd38145765744e80bcf44
adityaramki <aditya.ramki1@gmail.com>
2025-02-06 15:26:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,16 +9,16 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-          <Route path ="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-      </Routes>
-    </Router>
+    
+    <Routes>
+        <Route path ="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+    </Routes>
+  
       
     </>
   )
